8871254405f6c70ca04dc96ebe3efd2c86c6f80f
FujiiWill <fujiiwiliam@gmail.com>
2023-12-31 12:01:42 -0300

    Desafio concluido

---
+++ src/main/java/com/fujiiwill/desafiofe02/Desafiofe02Application.java
@@ -0,0 +1,13 @@
+package com.fujiiwill.desafiofe02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafiofe02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafiofe02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fujiiwill/desafiofe02/Desafiofe02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fujiiwill.desafiofe02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafiofe02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
